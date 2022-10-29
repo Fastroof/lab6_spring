8f1f676b1dc9bc18dba6a037547fd75f4a405a9e
oleksandrvolovik <olieksandr.volovik@gmail.com>
2022-10-29 16:11:47 +0300

    Fix user registration request validation.

--- src/main/java/com/fastroof/lab6_spring/pojo/UserRegistrationRequest.java
+++ src/main/java/com/fastroof/lab6_spring/pojo/UserRegistrationRequest.java
@@ -5,12 +5,21 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
 public class UserRegistrationRequest {
+    @NotNull
+    @Size(min = 1)
     private String email;
+    @NotNull
+    @Size(min = 1)
     private String password;
+    @NotNull
+    @Size(min = 1)
     private String fullName;
 }
